e3a341ec7dee7e9f27a3a76670b6dc5f101987b7
Sumeet Borole <sumeet21borole@gmail.com>
2026-09-24 00:29:26 +0530

    Add JWT authentication: register/login endpoints with bcrypt password hashing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -5,16 +5,16 @@ import { sendSuccess, sendError } from "../utils/responseHelper.js";
 
 const SALT_ROUNDS = 10;
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     //get req values
     const { email, password } = req.body;
 
     //validation check
-    if (!email && !email.include("@")) {
+    if (!email || !email.includes("@")) {
       return sendError(res, "Valid Email is require for registration", 400);
     }
-    if (!password && password.length < 8) {
+    if (!password || password.length < 8) {
       return sendError(res, "Valid Password is require for registration", 400);
     }
     //check if user exist or not in system
@@ -27,12 +27,12 @@ export const register = async (req, res) => {
     }
 
     //password hash
-    const hashpassword = bcrypt.hash(password, SALT_ROUNDS);
+    const hashpassword = await bcrypt.hash(password, SALT_ROUNDS);
 
     //insert user
     const result = await pool.query(
       `INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email, created_at`,
-      [email, passwordHash],
+      [email, hashpassword],
     );
     const user = result.rows[0];
 
@@ -45,6 +45,45 @@ export const register = async (req, res) => {
       { token, user: { id: user.id, email: user.email } },
       201,
     );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //validation check
+    if (!email || !password) {
+      return sendError(res, "Email and password are required", 400);
+    }
+
+    //check for user using email
+    const result = await pool.query("Select * from users where email=$1", [
+      email,
+    ]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return sendError(res, "provide right credentials", 401);
+    }
+
+    //password checking
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+    if (!passwordMatch) {
+      return sendError(res, "provide right credentials", 401);
+    }
+
+    //assign token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return sendSuccess(res, {
+      token,
+      user: { id: user.id, email: user.email },
+    });
   } catch (error) {
     next(error);
   }
